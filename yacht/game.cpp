24df75f8eedc04c5e8e3cc4ac31b0cc89682af3d
Fnhid <fnhid05@gmail.com>
2024-07-18 20:34:09 +0900

    set players only two(because VS don't support VLA, so some code issues occur)

--- yacht/game.cpp
+++ yacht/game.cpp
@@ -20,28 +20,44 @@ int Player::SetRank(int idx, int score) {
 }
 
 int game(int game_type) {
-	
+	string player_name;
+	system("mode con:cols=130 lines=40");
+	gotoxy(55, 5);
+	Player* players[2];
+
 	if (game_type == 1) {
-		int players_num;
-		string player_name;
-		system("mode con:cols=130 lines=40");
-		gotoxy(55, 5);
-		cout << "Please enter the number of players\n>> ";
-		cin >> players_num;
-		Player* players = new Player[players_num];
-		for (int i = 0; i < players_num; i++) {
+		
+		for (int i = 0; i < 2; i++) {
 			system("mode con:cols=130 lines=40");
-			gotoxy(55, 5);
+			gotoxy(45, 5);
 			cout << "Name of Player " << (i + 1) << ": ";
 			cin >> player_name;
-			players[i] = Player::SetPlayer(i, 0, player_name); 
+			players[i] = new Player(i, 0, player_name); 
+
+
+			
 		}
-		
 	} 
 	else if (game_type == 2) {
 		// game with computer
-		cout << "computer";
+		system("mode con:cols=130 lines=40");
+		gotoxy(45, 5);
+		cout << "Name of Player: ";
+		cin >> player_name;
+		players[0] = new Player(0, 0, player_name);
+		players[1] = new Player(1, 0, "computer");
+		
 	}
+	else {
+		// exception
+		return 0;
+	}
+
+
+	for (int i = 0; i < 2; i++) {
+		delete players[i];
+	}
+	
 	
 	return 0;
 }
